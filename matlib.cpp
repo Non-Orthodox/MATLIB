a3bd30cac14404aeee1cc06881a0affe5ca350fe
Blake <bctennis525@gmail.com>
2021-07-22 14:42:17 -0500

    Added resize function and cpp file

---
+++ matlib.cpp
@@ -0,0 +1,2 @@
+#include "matlib.h"
+
--- matlib.h
+++ matlib.h
@@ -1,5 +1,5 @@
-//#ifdef H_MATLIB
-//#define H_MATLIB
+#ifdef H_MATLIB
+#define H_MATLIB
 
 //namespace Matlib?
 
@@ -9,9 +9,13 @@ class matrix
         int n,m = 1; //n in numer of rows, m is number of columns
 
     public:
+    //Variable Members
+        //Matrix element values
         float** elements;
+        //Experimental, used to possibly modify the process of copying portions of matrices
+        int offset[2] = {0,0};
 
-        //Constructors
+    //Constructors
         matrix()
         {
             elements = new float* [1];
@@ -20,6 +24,7 @@ class matrix
         }
         matrix(int height, int width)
         {
+            //make sure to check if dimensions are positive numbers
             n = height;
             m = width;
             
@@ -34,7 +39,8 @@ class matrix
             }
         }
 
-        //Destructor
+
+    //Destructor
         ~matrix()
         {
             for(int i = 0; i < n; i++)
@@ -44,7 +50,15 @@ class matrix
             delete [] elements;
         }
 
-        //Member Functions
+
+    //Member Functions
+        //Returns element (r,c) as a float
+        float at(int r, int c)
+        {
+            return(elements[r][c]);
+        }
+        
+        //Returns size of matrix, as a 1x2 matrix representing [n m]
         matrix size()
         {
             matrix siz(1,2);
@@ -52,14 +66,74 @@ class matrix
             siz.elements[0][1] = m;
             return(siz);
         }
-        //void resize(int n, int m)
-        //matrix Transpose()
+
+        //Resizes the matrix. All indices which are not removed retain their elements. New elements are set to be 0. 
+        void resize(int rows, int cols)
+        {
+            matrix temp(rows,cols);
+            
+            //Copy all relevant elements to new matrix 
+            for(int i = 0; (i < n) && (i < rows); i++)
+            {
+                for(int j = 0; (j < m) && (j < cols); j++)
+                {
+                    temp.elements[i][j] = elements[i][j];
+                }
+            }
+            
+            //Delete old pointers
+            for(int k = 0; k < n; k++)
+            {
+                delete [] elements[k];
+            }
+            delete [] elements;
+
+            //Set new sizes
+            n = rows;
+            m = cols;
+
+            elements = temp.elements;
+            for(int l = 0; l < n; l++)
+            {
+                elements[l] = temp.elements[l];
+            }
+        }
+
+        //matrix push_back(matrix m)
+        //matrix T()
+        //matrix inv()
+        //eig()
 
 
-        //Operator Overloading
+    //Operator Overloading
+        matrix & operator=(const matrix &mat)
+        {
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
 
+                }
+            }
+        }
+
+        matrix & operator()(int a, int b)
+        {
+            if((a>=0)&&(b>=0)&&(a<n)&&(b<m))
+            {
+
+            }
+        }
 
 };
+
+//submatrix class used to assign portions of matrices
+class submatrix
+{
+
+};
+
 
 matrix rref(matrix*);
 
@@ -94,3 +168,4 @@ matrix:
 
 
 */
+#endif
